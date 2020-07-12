5529f5b03c0e0a13b24cb0586c60cf61d985de47
chenhe-henry <chenhe.elec@gmail.com>
2020-07-12 10:58:36 +1000

    Updates

---
+++ precache-manifest.202065938ab1d1abb9e9a9bc1b9fe04a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f608c38618d8c661c131a44afa6777ff",
+    "url": "/reactdeployongithub/index.html"
+  },
+  {
+    "revision": "7b9600213531a645efb8",
+    "url": "/reactdeployongithub/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e46eb87acc21237bb9d6",
+    "url": "/reactdeployongithub/static/js/2.c7fc020a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactdeployongithub/static/js/2.c7fc020a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9600213531a645efb8",
+    "url": "/reactdeployongithub/static/js/main.b2368e7d.chunk.js"
+  },
+  {
+    "revision": "bebe9f4fc35d91f3d100",
+    "url": "/reactdeployongithub/static/js/runtime-main.533d45bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdeployongithub/static/media/logo.5d5d9eef.svg"
+  }
+]);
